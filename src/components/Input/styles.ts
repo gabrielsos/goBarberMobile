ce9f43dede4f500d1b8f9c2128dffb0719d01b3c
Gabriel Silva <gabrielsos99@gmail.com>
2020-08-20 14:12:06 -0300

    initial commit

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,69 @@
+import styled, { css } from 'styled-components/native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isErrored: boolean;
+}
+
+interface IconsProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.View<ContainerProps>`
+  width: 100%;
+  height: 60px;
+  padding: 0 16px;
+  background: #232129;
+  border-radius: 10px;
+  margin-bottom: 8px;
+  border-width: 2px;
+  border-color: #232129;
+
+  flex-direction: row;
+  align-items: center;
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+    `}
+`;
+
+export const TextInput = styled.TextInput`
+  flex: 1;
+  color: #fff;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const Icon = styled(FeatherIcon)<IconsProps>`
+  margin-right: 16px;
+  color: #666360;
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
+  ${props =>
+    props.isErrored &&
+    css`
+      color: #c53030;
+    `}
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+    `}
+`;
